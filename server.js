128061a2884e92e18534f4e38c5ce1b2b5a7543a
Ryan <rlmcmillan12@gmail.com>
2022-05-20 10:59:46 -0400

    worked on sort and search

--- server.js
+++ server.js
@@ -13,6 +13,43 @@ app.get('/', (req, res) => res.send('Build the API!'))
 
 // Build Routes
 
+//returns all products
+app.get('/api/v1/products', (req, res) => {
+  const sort = req.query.sort || 'id'
+  const dir = req.query.dir || 'ASC'
+
+  res.json(productsService.findAll(sort, dir))
+});
+
+app.get('/api/v1/products/search', (req, res) => {
+  const key = req.query.key
+  const value = req.query.value
+  const sort = req.query.sort || 'id'
+  const dir = req.query.dir || 'ASC'
+
+  if (!key || !value) {
+    res.status(400).json({ error: 'Invalid search query must contain key and value' })
+    return
+  }
+  res.json(productsService.search(key, value, sort, dir))
+});
+//get product by id
+app.get('/api/v1/products/:id', (req, res) => {
+  const id = req.params.id
+  const product = productsService.findOneById(id)
+
+  if (isNaN(id)) {
+    res.status(400).json({ error: 'Invalid ID' })
+    return
+  }
+  if (!product) {
+    res.status(404).json({ error: 'Product not found' })
+    return
+  }
+  res.json(productsService.findOneById(id))
+});
+
+
 
 
 app.get('*', (req, res) => res.status(404).send('Page not found'))
